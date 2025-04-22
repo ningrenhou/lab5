01560a4e9bc9b63dc0181f13f35b5fef2d788878
Ningren-Hou <lillianforocad@gmail.com>
2025-04-21 23:50:28 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,46 +12,46 @@ var index = 0;
 //push: https://www.w3schools.com/jsref/jsref_push.asp
 //I was going to just use loadimage for this code, but realized I couldn't display more than one, so I added push, and everyone to make one loadimage and push.
 function preload(){
-    ajin = loadImage('/images/ajin.jpg');
+    ajin = loadImage('/lab5/images/ajin.jpg');
     alldaihaoyuan.push(ajin);
 
-    bugongping = loadImage('/images/bugongping.jpg');
+    bugongping = loadImage('/lab5/images/bugongping.jpg');
     alldaihaoyuan.push(bugongping);
 
-    buting = loadImage('/images/buting.jpg');
+    buting = loadImage('/lab5/images/buting.jpg');
     alldaihaoyuan.push(buting);
 
-    chuini = loadImage('/images/chuini.jpg');
+    chuini = loadImage('/lab5/images/chuini.jpg');
     alldaihaoyuan.push(chuini);
 
-    eihei = loadImage('/images/eihei.jpg');
+    eihei = loadImage('/lab5/images/eihei.jpg');
     alldaihaoyuan.push(eihei);
 
-    gege = loadImage('/images/gege.jpg');
+    gege = loadImage('/lab5/images/gege.jpg');
     alldaihaoyuan.push(gege);
 
-    glw = loadImage('/images/glw.jpg');
+    glw = loadImage('/lab5/images/glw.jpg');
     alldaihaoyuan.push(glw);
 
-    guofengxiao = loadImage('/images/guofengxiao.jpg');
+    guofengxiao = loadImage('/lab5/images/guofengxiao.jpg');
     alldaihaoyuan.push(guofengxiao);
 
-    maomao = loadImage('/images/maomao.jpg');
+    maomao = loadImage('/lab5/images/maomao.jpg');
     alldaihaoyuan.push(maomao);
 
-    nixiaozi = loadImage('/images/nixiaozi.jpg');
+    nixiaozi = loadImage('/lab5/images/nixiaozi.jpg');
     alldaihaoyuan.push(nixiaozi);
 
-    wink = loadImage('/images/wink.jpg');
+    wink = loadImage('/lab5/images/wink.jpg');
     alldaihaoyuan.push(wink);
 
-    wo = loadImage('/images/wo.jpg');
+    wo = loadImage('/lab5/images/wo.jpg');
     alldaihaoyuan.push(wo);
 
-    xixi = loadImage('/images/xixi.jpg');
+    xixi = loadImage('/lab5/images/xixi.jpg');
     alldaihaoyuan.push(xixi);
 
-    xuezhang = loadImage('/images/xuezhang.jpg');
+    xuezhang = loadImage('/lab5/images/xuezhang.jpg');
     alldaihaoyuan.push(xuezhang);
 
     data = loadJSON('data.json');
